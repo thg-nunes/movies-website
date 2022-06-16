2dd24a5c9270be1a149b51fb1dd0bc9dc8ab6c99
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-16 17:34:15 -0300

    feature: adiciona header na home page

---
+++ src/config/history/index.ts
@@ -0,0 +1,7 @@
+import { createBrowserHistory } from 'history'
+
+const history = createBrowserHistory()
+
+history.push('/home')
+
+export default history
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header } from '../../client/header'
+
+export const HomePage = () => {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  )
+}
