8319e583c5522a5d243e864c4fd8eb87bc3bd496
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-16 18:16:35 -0300

    adiciona diretorio com dados para tmdb

---
+++ src/config/endpoints-tmdb/index.ts
@@ -0,0 +1,3 @@
+export const ENDPOINT_TMDB = 'https://api.themoviedb.org/3'
+export const API_KEY_AND_LANGUAGE = 'edb3b0d9dbaf77dbd66037b73fe83f12&language=pt-BR'
+export const ENDPOINT_GET_IMAGE = 'https://image.tmdb.org/t/p'
