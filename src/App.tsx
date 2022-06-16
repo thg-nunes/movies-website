151c8d5d39b4d785105d54a82b46e50843f1d3a2
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-16 13:32:49 -0300

    feature: adiciona logica inicial para roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Navigate } from 'react-router'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+
+import { HomePage } from './pages/home'
+
 export default function App () {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to='/home' />} />
+        <Route path='/home' element={<HomePage />} />
+
+        <Route path='*' element={<Navigate to='/'/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
