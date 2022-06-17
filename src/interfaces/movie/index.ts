69034e3de8b2f281f2abc2446522bffca2f8be61
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-17 17:56:12 -0300

    feature: adiciona primeira lista de filmes na home page

---
+++ src/interfaces/movie/index.ts
@@ -0,0 +1,9 @@
+export interface MovieData {
+  title: string
+  releaseDate: Date
+  posterPath: string
+  voteAverage:number
+  popularity?: number
+  voteCount?: number
+  overview?: string
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../../client/header'
+import { MostWatchedMovies } from '../../client/movies/popular'
 
 export const HomePage = () => {
   return (
     <React.Fragment>
       <Header />
+      <MostWatchedMovies />
     </React.Fragment>
   )
 }
