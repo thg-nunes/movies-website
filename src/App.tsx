4801e6d521cb8e5a2a2cc3cbada560fa430666ee
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-21 14:00:16 -0300

    feature: cria rota para obter detalhes de um filme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate } from 'react-router'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 import { HomePage } from './pages/home'
+import { MovieDetail } from './pages/movie/details'
 
 export default function App () {
   return (
@@ -9,7 +10,7 @@ export default function App () {
       <Routes>
         <Route path='/' element={<Navigate to='/home' />} />
         <Route path='/home' element={<HomePage />} />
-
+        <Route path='/movie/:id/details' element={<MovieDetail />} />
         <Route path='*' element={<Navigate to='/'/>} />
       </Routes>
     </BrowserRouter>
--- src/config/history/index.ts
+++ src/config/history/index.ts
@@ -3,5 +3,6 @@ import { createBrowserHistory } from 'history'
 const history = createBrowserHistory()
 
 history.push('/home')
+history.push('/movie/datails')
 
 export default history
