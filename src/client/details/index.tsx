2895eae07b7350acef0910955897ea0922002084
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-22 10:47:39 -0300

    feature: layout da pagina de exibicao dos detalhes e filmes similares

---
+++ src/client/details/index.tsx
@@ -0,0 +1,36 @@
+import { useParams } from 'react-router'
+
+import { MovieDataToDetailst } from '../../interfaces/movie'
+import { MovieImageContainer } from '../../components/movie/details/image-container'
+import { OtherDetails } from '../../components/movie/details/other'
+import { SimilarMovies } from '../../components/movie/details/similar-movies'
+import { Details } from './styled'
+import { Header } from '../header'
+
+interface DataMovie {
+  dataMovie: MovieDataToDetailst
+}
+
+export const DetailsMovie = ({ dataMovie }:DataMovie) => {
+  const { poster_path, title, release_date, vote_average, overview, popularity, vote_count, genres } = dataMovie
+  const { id } = useParams()
+
+  return <Details>
+    <Header />
+    <MovieImageContainer
+      poster_path={poster_path}
+      title={title}
+      overview={overview}
+    />
+
+    <OtherDetails
+      release_date={release_date}
+      vote_average={vote_average}
+      popularity={popularity}
+      vote_count={vote_count}
+      genres={genres}
+    />
+
+    {id !== undefined && <SimilarMovies id={Number(id)} />}
+  </Details>
+}
---
+++ src/client/details/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Details = styled.section`
+  width: 100%;
+  height: 100vh;
+  overflow-y: scroll;
+`
