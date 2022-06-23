9d14a58841950cefe68c777d98a56e3d67be79e0
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-23 12:26:44 -0300

    feature: adiciona regra para redirecionar de pagina ao buscar por filme

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,14 +1,29 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import { SearchContainer } from './styled'
 import IconSearch from '../../images/icons/search.png'
 
-interface DataToSearchComponent {
+interface SearchProps {
   placeholder: string
 }
 
-export const Search = ({ placeholder }:DataToSearchComponent) => {
+export const Search = ({ placeholder }:SearchProps) => {
+  const navigate = useNavigate()
+  const [searchValue, setSearchValue] = useState<string | null>(null)
+
   return (
     <SearchContainer>
-      <input type={'text'} placeholder={placeholder} />
+      <input type={'text'} placeholder={placeholder}
+        onChange={(e) => {
+          setSearchValue(e.target.value)
+        }}
+        onKeyUp={(e) => {
+          if (searchValue !== null && e.code === 'Enter') {
+            navigate('/search/movie', { state: { searchValue } })
+          }
+        }}
+      />
       <img src={IconSearch} alt='icon-search' />
     </SearchContainer>
   )
